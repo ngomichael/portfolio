b0197f9e2a2e6ed3ef9323c40f14e4cb5e82ad2b
ngomichael <michael_ngo@live.com>
2017-12-23 00:02:19 -0800

    cleaned up files

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
 $(document).ready(function() {
+    //moves #titleBorder on mouse enter on #title
     $('#title').mouseenter(function() {
         titleBorder.style.transform = 'translate(260px, 75px)';
-    })
+    });
 
+    //moves #titleBorder on mouse leave on #title
     $('#title').mouseleave(function() {
         titleBorder.style.transform = 'translate(260px, -15px)';
-    })
+    });
 
+    //moves #titleInfo on mouse enter on #title
     $('#title').mouseenter(function() {
         titleInfo.style.transform = 'translate(-110%, 42px)';
-        // console.log($('#titleInfo'));
         $('#titleInfo').fadeTo('fast', '1');
-    })
+    });
 
+    //moves #titleInfo on mouse leave on #title
     $('#title').mouseleave(function() {
-        $('#titleInfo').fadeTo('fast', '0')
+        $('#titleInfo').fadeTo('fast', '0');
         titleInfo.style.transform = 'translate(-110%, 0px)';
-    })
-
-    // $('#resumeTab').mouseenter(function() {
-    //     titleInfo.style.transform = 'translate(-110%, 42px)';
-    //     // console.log($('#titleInfo'));
-    //     $('#titleInfo').fadeTo('fast', '1');
-    // })
-    //
-    // $('#resumeTab').mouseleave(function() {
-    //     $('#titleInfo').fadeTo('fast', '0')
-    //     titleInfo.style.transform = 'translate(-110%, 0px)';
-    // })
-
-
-    // $(".underLine").mouseenter(function() {
-    //     $(this).animate({
-    //         'borderBottom' :'2px solid #3399FF',
-    //         'width' :'46%'
-    //     });
-    // });
-    //
-    // $(".underLine").mouseleave(function() {
-    //     $(this).animate({
-    //         'borderBottom' :'2px solid #3399FF',
-    //         'width' :'0%'
-    //     });
-    // });
-
+    });
 
 });
